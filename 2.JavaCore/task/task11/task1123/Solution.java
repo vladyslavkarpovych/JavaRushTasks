11243e21be7ff026ac7d6947dbf7727855a6e4e2
Vladyslav Karpovych <65220119+vladyslavkarpovych@users.noreply.github.com>
2020-07-01 11:45:02 +0300

    Add files via upload

---
+++ 2.JavaCore/task/task11/task1123/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task11.task1123;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        int minX=inputArray[0];
+        int maxY=inputArray[0];
+        for (int i = 1; i < inputArray.length; i++) {
+            if (minX>inputArray[i]) minX=inputArray[i];
+            if (maxY<inputArray[i]) maxY=inputArray[i];
+        }
+        return new Pair<Integer, Integer>(minX, maxY);
+    }
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
