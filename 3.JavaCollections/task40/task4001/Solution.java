26012c9720eeaccdcf8d8cf44d95f8f3ae67b783
Vladyslav Karpovych <65220119+vladyslavkarpovych@users.noreply.github.com>
2020-07-01 12:20:38 +0300

    Add files via upload

---
+++ 3.JavaCollections/task40/task4001/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task40.task4001;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* 
+POST, а не GET
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost(new URL("http://requestb.in/1cse9qt1"), "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(URL url, String urlParameters) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+        OutputStream outputStream = connection.getOutputStream();
+        BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(outputStream, "UTF-8"));
+        writer.write(urlParameters);
+        writer.flush();
+        writer.close();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            response.append(responseLine);
+        }
+        bufferedReader.close();
+
+        System.out.println("Response: " + response.toString());
+    }
+
+}
